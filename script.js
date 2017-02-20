584c7b4747eee8513d34bcb44d5d415688edfe29
Tim <tim.lisiecki@gmail.com>
2017-02-20 18:02:41 -0500

    update header, page scroll animation

--- script.js
+++ script.js
@@ -73,6 +73,19 @@ function displayTeams() {
 displayTeams();
 
 
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$('a.page-scroll').bind('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: ($($anchor.attr('href')).offset().top - 50)
+    }, 1250, 'easeInOutExpo');
+    event.preventDefault();
+});
+
+
+
+
+
 
 /* Works for Patriots. Use as template for loooping through and displaying each team
 
