b3d3fde8245cecb5f5eabb8f8658a59617b5d647
Tim <tim.lisiecki@gmail.com>
2017-02-21 18:40:29 -0500

    add fade effect on scroll

--- script.js
+++ script.js
@@ -73,6 +73,25 @@ function displayTeams() {
 displayTeams();
 
 
+$(document).ready(function() {
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+        /* Check the location of each desired element */
+        $('.hide-item').each( function(i){
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},2000);
+                    
+            }   
+        });
+    });
+});
+
+
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $('a.page-scroll').bind('click', function(event) {
     var $anchor = $(this);
